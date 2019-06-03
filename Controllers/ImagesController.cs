4cc1a7ad5a4fdcb9b1b50a25cdd0082980ec0adf
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2019-06-03 10:57:28 +0900

    PDF表示のサンプル

--- Controllers/ImagesController.cs
+++ Controllers/ImagesController.cs
@@ -11,11 +11,22 @@ namespace mvcapp.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
-        // GET api/values
         [HttpGet("{file}.png")]
-        public IActionResult Get(string file)
+        public IActionResult PNGGet(string file)
         {
             string filePath = @"./assets/" + file + ".png";
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, (int)fs.Length);
+                return this.File(bytes, "application/octet-stream");
+            }
+        }
+
+        [HttpGet("{file}.pdf")]
+        public IActionResult PdfGet(string file)
+        {
+            string filePath = @"./assets/" + file + ".pdf";
             using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
                 var bytes = new byte[fs.Length];
---
+++ README.md
@@ -0,0 +1,7 @@
+# image-api
+
+## reference
+
+* 画像を取得可能なAPIの実装サンプル
+* HTML上にPDFを表示するサンプル
+  * [参考](https://qiita.com/katzueno/items/c490361c3274d0108e7d)
